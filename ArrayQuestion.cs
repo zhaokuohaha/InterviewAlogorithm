00ced54cbb09edfbc741a830698ed628b403d63e
zhaokuo <1014336691@qq.com>
2016-09-11 11:05:49 +0800

    确定一个字符串的所有字符是否完全不同

---
+++ ArrayQuestion.cs
@@ -0,0 +1,40 @@
+//数组类题目
+using System;
+
+namespace ConsoleApplication
+{
+    public class ArrayQuestion{
+        public void print(){
+
+        }
+    }
+
+    //确定一个字符串的所有字符是否完全不同 --> 假设只有26个字母
+    public class ISUniqueCharClass{
+        //用bool[]保存状态
+        public bool ISUniqueChar(string str){
+            if(str.Length > 256) return false;
+            bool[] res = new bool[26];
+            foreach (char c  in str)
+            {
+                int offset = c-'a';
+                if(res[offset]) return false;
+                res[offset] = true;
+            }
+            return true;
+        }
+
+        //用位保存状态
+        public bool ISUniqueChar2(string str){
+            if(str.Length > 26) return false;
+            int res = 0;
+            foreach(char c in str){
+                int val = c-'a';
+                if(res & (1 << val)) return false;
+                res |= (1 << val);
+            }
+            return true;
+        }
+    }
+    
+}
