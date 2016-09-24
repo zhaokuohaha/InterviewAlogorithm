8cf923bdda0aa94a8d126f1c1e421a17b00cbc34
zhaokuo <1014336691@qq.com>
2016-09-24 13:07:12 +0800

    更新后的dijkstra, 应该没有错了

--- Dijkstra.cs
+++ Dijkstra.cs
@@ -4,6 +4,8 @@ namespace ConsoleApplication
 {
     public class DijkstraClass
     {
+        //int max = int.MaxValue; 由于所有数据都是int, 如果这里用int.MaxValue, 底下 dist[u] + g[u,v]就有可能成为负数, 导致结果错误
+        int max = 99999;
         public void print(){
             /*
              /b—— d—— f
@@ -11,7 +13,7 @@ namespace ConsoleApplication
              \ c —— e —— g      
             */
             //int max = int.MaxValue;
-            int max = 9999;
+            //图源: http://img.blog.csdn.net/20130831184137437
             int[,] g = new int[,]{
                 {0,7,3,max,max,max,max},
                 {7,0,3,5,max,max,max},
@@ -21,41 +23,43 @@ namespace ConsoleApplication
                 {max,max,max,3,5,0,3},
                 {max,max,max,max,4,3,0}
             };
-            int[] dist = Dijkstra(0,g);
+            int[] dist = Dijkstra(3,g);
             foreach (int item in dist)
             {
                 Console.Write(item+" ");
             }
-            // for (int i = 0; i < g.GetLength(0); i++)
-            // {
-            //     for(int j=0; j<g.GetLength(1); j++){
-            //         Console.Write(g[i,j]+" ");
-            //     }
-            //     Console.WriteLine();
-            // }
+
+            //Dijkstra(0,g) : 0 6 3 6 7 9 11
+            //Dijkstra(3,g) : 6 5 3 0 2 3 6
+            //Dijkstra(6,g) : 11 11 8 6 4 3 0
         }
 
         public int[] Dijkstra(int root, int[,] g){
             int height = g.GetLength(0);
-            int width = g.GetLength(1);
-            int[] dist = new int[height];
-            bool[] vis = new bool[height];
+            int width = g.GetLength(1);         //二维数组的宽和高， 理论上应该是相等的， 这里为了演示下C#取值的方法
+            int[] dist = new int[height];       //存储最短路径的一维数组
+            bool[] vis = new bool[height];      //c存储已经被访问, 也就是确定了最短距离的点
+            //初始化
             for(int i=0; i<height; i++)
-                dist[i] = 9999;
-            dist[0] = 0;
-            vis[0] = true;
-            int min = 9999;
-            int min_index = 0;
-            for(int u=0; u<height; u++){
-                for(int v=0; v<width; v++){
-                    if(u==v || vis[v]) continue;
-                    dist[v] = Math.Min(dist[v], dist[u]+g[u,v]);
-                    if(dist[v] < min){
-                        min = dist[v];
-                        min_index = v;
+                dist[i] = g[root,i];
+            dist[root] = 0;
+            vis[root] = true;
+            for(int i=0; i<height; i++){//大循环表示需要刷新height轮, 每轮能够确定一个点的最小路径, 里面小循环一个找最小值, 一个更新路径
+                //min: 每次遍历过程的最小距离, u: 取得最小距离的点
+                //每次循环都需要把min初始化, 如果min放在总循环外边, 比如当第一次遍历之后min的值变得很小,  后面min就不会更新导致会出错, u倒不一定, min更新后u自然更新, 放在一起只是为了好理解
+                int u=root;
+                int min = max;
+                for(int j=0; j<dist.Length; j++){
+                    if(!vis[j] && dist[j] < min){
+                        min = dist[j];
+                        u=j;
                     }
                 }
-                vis[min_index] = true;
+                vis[u] = true;
+
+                for(int v=0; v<width; v++){
+                    dist[v] = Math.Min(dist[v], dist[u]+g[u,v]);
+                }
             }
             return dist;    
         }
