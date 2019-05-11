7ce01dd00861d5a02712f287063a1e3c2ba4fe9b
zhaokuohaha <1014336691@qq.com>
2019-05-11 23:47:20 +0800

    comment: get_all_palindrome

--- Common/TreeNode.cs
+++ Common/TreeNode.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace ConsoleApplication
 {
+    /// <summary>
+    /// 二叉树结点
+    /// </summary>
     public class TreeNode{
         public int val{get;set;}
         public TreeNode left{get;set;}
--- InterviewQuestion/GetAllPalindrome.cs
+++ InterviewQuestion/GetAllPalindrome.cs
@@ -1,8 +1,13 @@
-//东方财富笔试题:找出所有的回文子串
+/// <summary>
+/// 东方财富笔试题:找出所有的回文子串
+/// 分两种情况, 1. s[i] 为字串的中间， 2. s[i] = s[i+1] 字串关于二者之间堆成
+/// 实现：遍历字符串， 分以上两种情况， 找出字串， 以第一中情况为例子：
+/// 从位置 i 开始， 设置游标l, r 分别网左右查询， 直到 s[l] != s[r], 如果长度 > 1 则为有效字串 p
+/// 如果已经找到的字串中不含 p ， 则 res.Add(p)
+/// </summary>
 
 using System;
 using System.Collections.Generic;
-
 
 namespace ConsoleApplication{
     public class GetAllPalindromeClass{
